950a6daf9a56f8c6c33f8882346fdacdec0cd5a1
Omar-Brika <brika.omar.04@gmail.com>
2025-12-08 19:09:56 +0100

    Fixed reading config bug

--- src/config.c
+++ src/config.c
@@ -9,21 +9,24 @@ config read_config() {
   FILE *file = fopen("data/config.txt", "r");
   if (file == NULL) {
     printf("couldn't open file !!!! wenou ya 7mayza\n");
-  }
-
-  int items = fscanf(file, "seuil_min=%lf\nseuil_max=%lf\nintervalle_mesure=%d",
-                     &seuil_min, &seuil_max, &intervalle_mesure);
-  fclose(file);
-
-  if (items == 3) {
-    printf("these are the values: \n");
-    printf("seuil_min: %1f\n", seuil_min);
-    printf("seuil_max: %1f\n", seuil_max);
-    printf("intervalle_mesure: %d seconds\n", intervalle_mesure);
+    printf("fallback to defaults.\n");
   } else {
-    printf("couldnt read config. fallback to defaults.\n");
-  }
 
+    int items =
+        fscanf(file, "seuil_min=%lf\nseuil_max=%lf\nintervalle_mesure=%d",
+               &seuil_min, &seuil_max, &intervalle_mesure);
+
+    fclose(file);
+
+    if (items == 3) {
+      printf("these are the values: \n");
+      printf("seuil_min: %1f\n", seuil_min);
+      printf("seuil_max: %1f\n", seuil_max);
+      printf("intervalle_mesure: %d seconds\n", intervalle_mesure);
+    } else {
+      printf("couldnt read config. fallback to defaults.\n");
+    }
+  }
   config current_config = {seuil_min, seuil_max, intervalle_mesure};
 
   return current_config;
