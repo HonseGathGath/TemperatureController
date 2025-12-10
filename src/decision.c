04ae902b8710654b983592d08698ad2ad4890a4f
HonseGathGath <lassouedghaith15@gmail.com>
2025-12-10 17:06:07 +0000

    some minor changes

--- src/decision.c
+++ src/decision.c
@@ -3,13 +3,13 @@
 AlertLevel check_temperature(double temperature, const config *config) {
   double ecart_min = config->seuil_min - temperature;
   double ecart_max = temperature - config->seuil_max;
-  
+
   if (ecart_min > 10 || ecart_max > 10) {
-    return ALERT_NIVEAU3;  // Critique: +10°C dépassement
+    return ALERT_NIVEAU3; // Critique: +10°C dépassement
   } else if (ecart_min > 5 || ecart_max > 5) {
-    return ALERT_NIVEAU2;  // Modéré: 5-10°C dépassement
+    return ALERT_NIVEAU2; // Modéré: 5-10°C dépassement
   } else if (ecart_min > 0 || ecart_max > 0) {
-    return ALERT_NIVEAU1;  // Léger: 0-5°C dépassement
+    return ALERT_NIVEAU1; // Léger: 0-5°C dépassement
   } else {
     return ALERT_NONE;
   }
@@ -18,7 +18,7 @@ AlertLevel check_temperature(double temperature, const config *config) {
 char *print_alert(AlertLevel alert) {
   switch (alert) {
   case ALERT_NONE:
-    return "jawwek behi";
+    return "pas de problème";
   case ALERT_NIVEAU1:
     return "Avertissement léger";
   case ALERT_NIVEAU2:
